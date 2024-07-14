eb062f026084bb5096d806899d7dda3daed65b6c
ldauga <ldauga.student@42lyon.fr>
2024-07-14 18:09:52 +0200

    true histogram

--- histogram.py
+++ histogram.py
@@ -1,8 +1,102 @@
+import math
 import os
 import sys
-from Utils.file_utils import get_houses_std_value, open_file, parse_file, parse_file_data_by_house
-from Utils.my_math import my_len, my_sum
+
+from matplotlib.widgets import Button
+from Utils.file_utils import (
+    open_file,
+    parse_file,
+    parse_file_data_by_house,
+    transform_file_data,
+)
+from Utils.my_math import my_len, my_max, my_min
 import matplotlib.pyplot as plt
+
+class HistogramCourses:
+    def __init__(self, houses_data, keys):
+        self.houses_data = houses_data
+        self.keys = keys
+        self.index = 0
+        self.fig, self.ax  = plt.subplots()
+        self.update_plot()
+        self.create_buttons()
+
+    def update_plot(self):
+        key_selected = self.keys[self.index]
+        
+        self.ax.cla()  # Clear the axis once before plotting all histograms
+        
+        for house, data, color in [
+            ("Slytherin", self.houses_data["Slytherin"], "green"),
+            ("Ravenclaw", self.houses_data["Ravenclaw"], "blue"),
+            ("Gryffindor", self.houses_data["Gryffindor"], "red"),
+            ("Hufflepuff", self.houses_data["Hufflepuff"], "yellow"),
+        ]:
+            course_data = [item[key_selected] for item in data]
+            
+            item_number = len(course_data)
+            class_number = round(1 + math.log2(item_number))
+            
+            min_value = min(course_data)
+            max_value = max(course_data)
+            
+            amplitude = max_value - min_value
+            
+            class_amplitude = amplitude / class_number
+            interval = [min_value + i * class_amplitude for i in range(class_number)]
+            
+            frequency = []
+            for i in range(len(interval) - 1):
+                number_in_interval = len([item for item in course_data if interval[i] <= item <= interval[i+1]])
+                
+                frequency.append(number_in_interval)
+            
+            self.ax.hist(course_data, bins=class_number, range=[min_value, max_value], color=color, alpha=0.5, label=house)
+                
+        self.ax.legend()
+        self.ax.set_title(f"Histogram for {key_selected}")
+        self.ax.set_xlim(
+            min_value - abs(max_value) * 0.1,
+            max_value + abs(max_value) * 0.1,
+        )
+        self.fig.canvas.draw_idle()
+
+    def next_key(self, event):
+        self.index = (self.index + 1) % len(self.keys)
+        self.update_plot()
+
+    def prev_key(self, event):
+        self.index = (self.index - 1) % len(self.keys)
+        self.update_plot()
+
+    def create_buttons(self):
+        axprev = plt.axes([0.7, 0.01, 0.1, 0.075])
+        axnext = plt.axes([0.81, 0.01, 0.1, 0.075])
+
+        self.bnext = Button(axnext, "Next")
+        self.bnext.on_clicked(self.next_key)
+
+        self.bprev = Button(axprev, "Prev")
+        self.bprev.on_clicked(self.prev_key)
+
+
+    def next_key(self, event):
+        self.index = (self.index + 1) % len(self.keys)
+        self.update_plot()
+
+    def prev_key(self, event):
+        self.index = (self.index - 1) % len(self.keys)
+        self.update_plot()
+
+    def create_buttons(self):
+        axprev = plt.axes([0.7, 0.01, 0.1, 0.075])
+        axnext = plt.axes([0.81, 0.01, 0.1, 0.075])
+
+        self.bnext = Button(axnext, "Next")
+        self.bnext.on_clicked(self.next_key)
+
+        self.bprev = Button(axprev, "Prev")
+        self.bprev.on_clicked(self.prev_key)
 
 
 if my_len(sys.argv) < 2:
@@ -12,40 +106,45 @@ else:
         print("File not found.")
     else:
         DATASET_PATH = sys.argv[1]
-        
+
         with open_file(sys.argv[1]) as file:
             file_data = parse_file(file)
-            
-            
+
             houses_data = parse_file_data_by_house(file_data)
-            
-            gryffindor_data = get_houses_std_value(houses_data["Gryffindor"])
-            ravenclaw_data = get_houses_std_value(houses_data["Ravenclaw"])
-            slytherin_data = get_houses_std_value(houses_data["Slytherin"])
-            hufflepuff_data = get_houses_std_value(houses_data["Hufflepuff"])
-            
-            
-            fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
-            fig.suptitle('Courses grade for each Houses')
+            file_data = transform_file_data(file_data)
 
-            ax1.plot([key.split(' ')[0] for key in gryffindor_data], [gryffindor_data[key] for key in gryffindor_data], color='red' )
-            ax1.tick_params(axis='x', labelrotation=90)
-            ax1.set_title("Gryffindor Std Courses Grade")
+            # gryffindor_data = get_houses_std_value(houses_data["Gryffindor"])
+            # ravenclaw_data = get_houses_std_value(houses_data["Ravenclaw"])
+            # slytherin_data = get_houses_std_value(houses_data["Slytherin"])
+            # hufflepuff_data = get_houses_std_value(houses_data["Hufflepuff"])
 
-            ax2.plot([key.split(' ')[0] for key in ravenclaw_data], [ravenclaw_data[key] for key in ravenclaw_data], color='blue' )
-            ax2.tick_params(axis='x', labelrotation=90)
-            ax2.set_title("Ravenclaw Std Courses Grade")
+            # print(houses_data)
+            # for house in houses_data:
+                # data = houses_data[house]
+                # print(data)
+                # class_number = 1 + math.log2()
+            # fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
+            # fig.suptitle("Courses grade for each Houses")
 
-            ax3.plot([key.split(' ')[0] for key in slytherin_data], [slytherin_data[key] for key in slytherin_data], color='green' )
-            ax3.tick_params(axis='x', labelrotation=90)
-            ax3.set_title("Slytherin Std Courses Grade")
+            # ax1.plot([key.split(' ')[0] for key in gryffindor_data], [gryffindor_data[key] for key in gryffindor_data], color='red' )
+            # ax1.tick_params(axis='x', labelrotation=90)
+            # ax1.set_title("Gryffindor Std Courses Grade")
 
-            ax4.plot([key.split(' ')[0] for key in hufflepuff_data], [hufflepuff_data[key] for key in hufflepuff_data], color='yellow' )
-            ax4.tick_params(axis='x', labelrotation=90)
-            ax4.set_title("Hufflepuff Std Courses Grade")
+            # ax2.plot([key.split(' ')[0] for key in ravenclaw_data], [ravenclaw_data[key] for key in ravenclaw_data], color='blue' )
+            # ax2.tick_params(axis='x', labelrotation=90)
+            # ax2.set_title("Ravenclaw Std Courses Grade")
 
+            # ax3.plot([key.split(' ')[0] for key in slytherin_data], [slytherin_data[key] for key in slytherin_data], color='green' )
+            # ax3.tick_params(axis='x', labelrotation=90)
+            # ax3.set_title("Slytherin Std Courses Grade")
+
+            # ax4.plot([key.split(' ')[0] for key in hufflepuff_data], [hufflepuff_data[key] for key in hufflepuff_data], color='yellow' )
+            # ax4.tick_params(axis='x', labelrotation=90)
+            # ax4.set_title("Hufflepuff Std Courses Grade")
+            keys_possible = [
+                key
+                for key in file_data[0]
+                if isinstance(file_data[0][key], float) and key != "Index"
+            ]
+            scatter_plot = HistogramCourses(houses_data, keys_possible)
             plt.show()
-
-
-            
-            
--- scatter_plot.py
+++ scatter_plot.py
@@ -2,50 +2,192 @@ import os
 import sys
 
 from matplotlib.widgets import Button
-from Utils.file_utils import open_file, parse_file, transform_file_data
+from Utils.file_utils import (
+    open_file,
+    parse_file,
+    parse_file_data_by_house,
+    transform_file_data,
+)
 from Utils.my_math import my_len, my_max, my_min
 import matplotlib.pyplot as plt
 
-class ScatterPlot:
-    def __init__(self, file_data, keys):
-        self.file_data = file_data
+
+class ScatterPlotCourses:
+    def __init__(self, houses_data, keys):
+        self.houses_data = houses_data
         self.keys = keys
+        self.index = 0
+        self.fig, self.ax = plt.subplots()
+        self.scatterSlytherin = None
+        self.scatterRavenclaw = None
+        self.scatterGryffindor = None
+        self.scatterHufflepuff = None
+        self.update_plot()
+        self.create_buttons()
+
+    def update_plot(self):
+        key_selected = self.keys[self.index]
+
+        if self.scatterSlytherin:
+            self.scatterSlytherin.remove()
+        if self.scatterRavenclaw:
+            self.scatterRavenclaw.remove()
+        if self.scatterGryffindor:
+            self.scatterGryffindor.remove()
+        if self.scatterHufflepuff:
+            self.scatterHufflepuff.remove()
+
+        min_value = None
+        max_value = None
+
+        for house, data, color in [
+            ("Slytherin", self.houses_data["Slytherin"], "green"),
+            ("Ravenclaw", self.houses_data["Ravenclaw"], "blue"),
+            ("Gryffindor", self.houses_data["Gryffindor"], "red"),
+            ("Hufflepuff", self.houses_data["Hufflepuff"], "yellow"),
+        ]:
+            x_data = [i for i in range(my_len(data))]
+            y_data = [item[key_selected] for item in data]
+
+            if min_value == None or my_min(y_data) < min_value:
+                min_value = my_min(y_data)
+            if max_value == None or my_max(y_data) > max_value:
+                max_value = my_max(y_data)
+
+            if house == "Slytherin":
+                self.scatterSlytherin = self.ax.scatter(
+                    x_data, y_data, label=house, color=color
+                )
+            if house == "Ravenclaw":
+                self.scatterRavenclaw = self.ax.scatter(
+                    x_data, y_data, label=house, color=color
+                )
+            if house == "Gryffindor":
+                self.scatterGryffindor = self.ax.scatter(
+                    x_data, y_data, label=house, color=color
+                )
+            if house == "Hufflepuff":
+                self.scatterHufflepuff = self.ax.scatter(
+                    x_data, y_data, label=house, color=color
+                )
+            self.ax.legend()
+            self.ax.set_title(f"Scatter plot for {key_selected} of each houses")
+        self.ax.set_ylim(
+            min_value - abs(max_value) * 0.1,
+            max_value + abs(max_value) * 0.1,
+        )
+        self.fig.canvas.draw_idle()
+
+    def next_key(self, event):
+        self.index = (self.index + 1) % len(self.keys)
+        self.update_plot()
+
+    def prev_key(self, event):
+        self.index = (self.index - 1) % len(self.keys)
+        self.update_plot()
+
+    def create_buttons(self):
+        axprev = plt.axes([0.7, 0.01, 0.1, 0.075])
+        axnext = plt.axes([0.81, 0.01, 0.1, 0.075])
+
+        self.bnext = Button(axnext, "Next")
+        self.bnext.on_clicked(self.next_key)
+
+        self.bprev = Button(axprev, "Prev")
+        self.bprev.on_clicked(self.prev_key)
+
+
+class ScatterPlotHouses:
+    def __init__(self, houses_data):
+        self.houses_data = houses_data
+        self.display_arithmancy = False
         self.index = 0
         self.fig, self.ax = plt.subplots()
         self.scatter = None
         self.update_plot()
         self.create_buttons()
-        
+
     def update_plot(self):
-        key_selected = self.keys[self.index]
-        x_data = [i for i in range(my_len(self.file_data))]
-        y_data = [item[key_selected] for item in self.file_data]
-        
+        house_selected = [
+            "Slytherin",
+            "Ravenclaw",
+            "Gryffindor",
+            "Hufflepuff",
+        ][self.index]
+
         if self.scatter:
             self.scatter.remove()
-        self.scatter = self.ax.scatter(x_data, y_data, label=key_selected)
-        self.ax.set_ylim(my_min(y_data) - abs(my_max(y_data) *.1), my_max(y_data) + abs(my_max(y_data) *.1))
-        self.ax.legend()
-        self.ax.set_title(f'Scatter plot for {key_selected}')
+
+        min_value = None
+        max_value = None
+
+        data = self.houses_data[house_selected]
+        
+        x_data = [i for i in range(my_len(data) * (my_len(data[0]) - (1 if not self.display_arithmancy else 0)))]
+        y_data = []
+        for item in data:
+            for key in item:
+                if key == 'Arithmancy' and not self.display_arithmancy:
+                    pass
+                else:
+                    y_data.append(item[key])
+
+        if min_value == None or my_min(y_data) < min_value:
+            min_value = my_min(y_data)
+        if max_value == None or my_max(y_data) > max_value:
+            max_value = my_max(y_data)
+
+        if house_selected == "Slytherin":
+            self.scatter = self.ax.scatter(
+                x_data, y_data, label=house_selected, color="green"
+            )
+        if house_selected == "Ravenclaw":
+            self.scatter = self.ax.scatter(
+                x_data, y_data, label=house_selected, color="blue"
+            )
+        if house_selected == "Gryffindor":
+            self.scatter = self.ax.scatter(
+                x_data, y_data, label=house_selected, color="red"
+            )
+        if house_selected == "Hufflepuff":
+            self.scatter = self.ax.scatter(
+                x_data, y_data, label=house_selected, color="yellow"
+            )
+            self.ax.legend()
+            self.ax.set_title(f"Scatter plot for {house_selected} of each houses")
+        self.ax.set_ylim(
+            min_value - abs(max_value) * 0.1,
+            max_value + abs(max_value) * 0.1,
+        )
         self.fig.canvas.draw_idle()
-    
+
     def next_key(self, event):
-        self.index = (self.index + 1) % len(self.keys)
+        self.index = (self.index + 1) % 4
         self.update_plot()
-    
+
     def prev_key(self, event):
-        self.index = (self.index - 1) % len(self.keys)
+        self.index = (self.index - 1) % 4
         self.update_plot()
-    
+        
+    def toggle_display_arithmancy(self, event):
+        self.display_arithmancy = not self.display_arithmancy
+        self.update_plot()
+
     def create_buttons(self):
         axprev = plt.axes([0.7, 0.01, 0.1, 0.075])
         axnext = plt.axes([0.81, 0.01, 0.1, 0.075])
         
-        self.bnext = Button(axnext, 'Next')
-        self.bnext.on_clicked(self.next_key)
+        axtoggle = plt.axes([0.1, 0.01, 0.3, 0.075])
         
-        self.bprev = Button(axprev, 'Prev')
+        self.btoggle = Button(axtoggle, "Toggle Display Arithmancy")
+        self.btoggle.on_clicked(self.toggle_display_arithmancy)
+
+        self.bnext = Button(axnext, "Next")
+        self.bnext.on_clicked(self.next_key)
+
+        self.bprev = Button(axprev, "Prev")
         self.bprev.on_clicked(self.prev_key)
+
 
 if my_len(sys.argv) < 2:
     print("Need arg.")
@@ -54,15 +196,22 @@ else:
         print("File not found.")
     else:
         DATASET_PATH = sys.argv[1]
-        
+
         with open_file(sys.argv[1]) as file:
             file_data = parse_file(file)
 
+            houses_data = parse_file_data_by_house(file_data)
+
             file_data = transform_file_data(file_data)
 
-            keys_possible = [key for key in file_data[0] if isinstance(file_data[0][key], float) and key != 'Index']
+            keys_possible = [
+                key
+                for key in file_data[0]
+                if isinstance(file_data[0][key], float) and key != "Index"
+            ]
             current_key_index = 0
 
-            scatter_plot = ScatterPlot(file_data, keys_possible)
+            scatter_plot = ScatterPlotCourses(houses_data, keys_possible)
+            scatter_plot_house = ScatterPlotHouses(houses_data)
+
             plt.show()
-            
